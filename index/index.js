238e0631c8fe56bb5f0f503772aa88289fdf068c
Sanjarbek012 <sanjarbekxaytboyevv4@gmail.com>
2026-04-20 14:25:59 +0500

    init

---
+++ index/index.js
@@ -0,0 +1,48 @@
+// uyga vazifa
+// 4-masala
+
+//  const fs = require("fs")
+// fs.mkdir("html", { recursive: true }, (err) => {
+//   if (err) throw err
+
+//   fs.writeFile("html/index.html", "<h1>Hello World</h1>", (err) => {
+//     if (err) throw err
+//     console.log("html papka va index.html yaratildi!")
+//    })
+//  })
+
+//   const fs = require("fs")
+// fs.mkdir("static", { recursive: true }, (err) => {
+//   if (err) throw err
+
+//  fs.writeFileSync("static/script.js", "console.log('Hello World')")
+//   fs.writeFileSync("static/script.ts", "let msg: string = 'Hello TypeScript';")
+
+//   fs.writeFileSync("static/script.js", "console.log('Hello World')")
+//   fs.writeFileSync("static/script.ts", "let msg: string = 'Hello TypeScript';")
+//   fs.writeFileSync("static/style.css", "body { font-family: Arial; }")
+
+//   fs.writeFileSync("static/go.mod", "module golang-todo-app\n\ngo 1.20")
+//   fs.writeFileSync("static/go.sum", "")
+//   fs.writeFileSync("static/main.go", "package main\n\nfunc main() {\n\tprintln(\"Hello Go\")\n}")
+
+//   fs.writeFileSync("static/README.md", "# Golang Todo App")
+
+//   fs.writeFileSync(
+//     "static/tsconfig.json",
+//     JSON.stringify(
+//       {
+//         compilerOptions: {
+//           target: "ES6",
+//           module: "commonjs",
+//           strict: true,
+//           esModuleInterop: true
+//         }
+//       },
+//       null,
+//       2
+//     )
+//   )
+
+//   console.log("Barcha fayllar yaratildi!")
+// })
---
+++ static/README.md
@@ -0,0 +1,1 @@
+# Golang Todo App
